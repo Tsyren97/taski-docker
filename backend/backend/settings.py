7288967efbe9adaa527d2187a62c668f6b0f0e37
Tsyren Zhalsapov <zhalsapov97@mail.ru>
2025-12-06 03:29:12 +0800

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'mytaskidocker.ddns.net']
-
+ALLOWED_HOSTS = ['51.250.23.197', '127.0.0.1', 'localhost', 'mytaskidocker.ddns.net']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
